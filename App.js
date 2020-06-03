7de0a0ab84dc3baac275067e1e5c4dc6d8e1636d
dashty94 <dashty.fr@gmail.com>
2020-06-03 23:17:38 +0300

    Add text input component

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
-import React, { Component } from "react"
-import { View } from "react-native"
+import React, { useState } from "react"
+import { View, TextInput } from "react-native"
 import WelcomeScreen from "./app/screens/WelcomeScreen"
 import ViewImageScreen from "./app/screens/ViewImageScreen"
 import LearningAssets from "./app/learningAssets/StylingText"
@@ -12,7 +12,14 @@ import Icon from "./app/components/Icon"
 import ListItem from "./app/components/ListItem"
 import AccountScreen from "./app/screens/AccountScreen"
 import ListingsScreen from "./app/screens/ListingsScreen"
+import AppTextInput from "./app/components/AppTextInput"
 
 export default function App() {
-    return <ListingsScreen />
+    const [firstName, setFirstName] = useState("")
+
+    return (
+        <Screen>
+            <AppTextInput placeholder="First name" icon="email" />
+        </Screen>
+    )
 }
